988c97e9e67a8f041e383070bf0ec59f07d10ba6
Gutenberg Neto <gutenbergn@gmail.com>
2024-05-06 17:24:09 -0300

    adding Package.swift

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TTTAttributedLabel",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "TTTAttributedLabel",
+            targets: ["TTTAttributedLabel"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/gutenbergn/FuzeUtils.git", exact: "0.6.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TTTAttributedLabel",
+            dependencies: ["FuzeUtils"],
+            path: "TTTAttributedLabel",
+            linkerSettings: [
+                .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreGraphics", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("QuartzCore", .when(platforms: [.iOS, .tvOS]))
+            ]
+        ),
+        //.testTarget(
+        //    name: "FuzeUtilsTests",
+        //    dependencies: ["FuzeUtils"]),
+    ]
+)
