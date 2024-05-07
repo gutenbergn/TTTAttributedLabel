a68bfc75c05bae6bd60e0a5def20d17a9c14ca47
Gutenberg Neto <gutenbergn@gmail.com>
2024-05-07 13:01:47 -0300

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/gutenbergn/FuzeUtils.git", exact: "0.6.0"),
+        .package(url: "https://github.com/gutenbergn/FuzeUtils.git", .upToNextMajor(from: "0.6.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
